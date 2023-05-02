1e173c61f373321131fdd595c203029a087fb2f1
Irina <rakhimovairina1@gmail.com>
2023-05-02 19:56:50 +0300

    Add JavaScript

---
+++ javascript.js
@@ -0,0 +1,27 @@
+openPdf = function() {
+    window.open('./Style/CV.pdf', '_blank');
+};
+
+openHTML = function() {
+    window.open('./Style/HTML sertificate.png')
+}
+
+openCSS = function() {
+    window.open('./Style/CSS sertificate.png')
+}
+
+openJS = function() {
+    window.open('./Style/JavaScript sertificate.png')
+}
+
+openProject1 = function() {
+    window.open('https://github.com/IrinaRakhimova/Portfolio')
+}
+
+openProject2 = function() {
+    window.open('https://github.com/IrinaRakhimova/Finnish_club')
+}
+
+openProject3 = function() {
+    window.open('https://github.com/IrinaRakhimova/IrinaRakhimova.github.io')
+}
